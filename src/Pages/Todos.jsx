f7fcbc9f21b0c3d10941ea3b900e5450bda2b912
Hira-Yuki <cjsdlf44@gmail.com>
2023-05-05 21:19:35 +0900

    todos페이지 구성 생성, 사이드바에 링크 추가

--- src/Pages/Todos.jsx
+++ src/Pages/Todos.jsx
@@ -1,31 +1,66 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function Todos() {
   const todoStore = useSelector((state) => state).todoReducer;
-  let count = 1
+  let count = 1;
 
   return (
     <div>
-      <table>
-        <th>번호</th>
-        <th>제목</th>
-        {todoStore.map((item) => {
-          return (
-            <tr>
-              <td key={item.id}>{count++}</td>
-              <td>
-                <Link to={`/todos/${item.id}`}>
-                  <span>{item.title}</span>
-                </Link>
-              </td>
-            </tr>
-          );
-        })}
-      </table>
+      <Link to={"/"}>홈으로 돌아가기</Link>
+      <StTable>
+        <colgroup>
+          <col style={{ width: "300px" }} />
+          <col style={{ width: "600px" }} />
+        </colgroup>
+        <thead>
+          <tr>
+            <StNumberColumn>번호</StNumberColumn>
+            <StTitleColumn>제목</StTitleColumn>
+          </tr>
+        </thead>
+        <tbody>
+          {todoStore.map((item) => {
+            return (
+              <tr key={item.id}>
+                <StNumberColumn>{count++}</StNumberColumn>
+                <StTitleColumn>
+                  <Link to={`/todos/${item.id}`}>
+                    <StTitleLink>{item.title}</StTitleLink>
+                  </Link>
+                </StTitleColumn>
+              </tr>
+            );
+          })}
+        </tbody>
+      </StTable>
     </div>
   );
 }
 
 export default Todos;
+
+const StTable = styled.table`
+  border-collapse: collapse;
+  border: 2px solid black;
+  width: 900px;
+`;
+
+const StNumberColumn = styled.th`
+  border: 1px solid black;
+  text-align: center;
+  width: 300px;
+`;
+
+const StTitleColumn = styled.td`
+  border: 1px solid black;
+  text-align: center;
+  width: 600px;
+`;
+
+const StTitleLink = styled.span`
+  display: block;
+  text-align: center;
+`;
